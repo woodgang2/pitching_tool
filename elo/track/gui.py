698d3d083da8e5d1722315d3a72a8d77a187ffef
gang <wsg9mf@virginia.edu>
2024-03-22 00:00:56 -0400

    Improvements to user interface

--- elo/track/gui.py
+++ elo/track/gui.py
@@ -87,13 +87,13 @@ if 'team_flag' not in st.session_state:
 # Your database initialization
 driver = database_driver.DatabaseDriver()
 stuff_driver = stuff_plus.Driver('radar2.db', 'radar_data')
-
+counter = 0
 # Update dataset button
 col1, col2, space = st.columns([2, 2, 2])
 with col1:
     update = st.button("Update Dataset", key='update_dataset', type = 'primary')
     if update:
-        st.write ("I'm a placeholder button")
+        st.write (f"I'm a placeholder button. You've pushed me {++counter} times - want to do it again?")
         # st.write('Updating. May take a while')
         # driver.read_data()
         # stuff_plus.process_data()
